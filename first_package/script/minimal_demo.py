0a6e3ab7681613de9b5162cbfda8228dce7bb1f8
zeidk <zeidk@umd.edu>
2024-02-17 14:38:29 -0500

    added exception handling

--- first_package/script/minimal_demo.py
+++ first_package/script/minimal_demo.py
@@ -7,8 +7,14 @@ def main(args=None):
     rclpy.init(args=args)
     node = Node("minimal_py")
     node.get_logger().info(f"Hello from {node.get_name()}")
-    rclpy.spin(node)
-    rclpy.shutdown()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        # Log a message when the node is manually terminated
+        node.get_logger().warn("Keyboard interrupt detected")
+    finally:
+        # Shut down the ROS 2 Python client library
+        rclpy.shutdown()
     
 if __name__ == "__main__":
     main()
