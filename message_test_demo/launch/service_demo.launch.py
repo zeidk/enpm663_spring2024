dc58a27b8cd1823290da2407736885d67a498b4e
zeid <zeid.kootbally@nist.gov>
2024-03-09 13:39:56 -0500

    Add publisher, server (C++ and Python), client (C++), and service calls from timer (C++)

--- message_test_demo/launch/service_demo.launch.py
+++ message_test_demo/launch/service_demo.launch.py
@@ -7,16 +7,18 @@ from launch_ros.actions import Node
 def generate_launch_description():
     ld = LaunchDescription()
 
+    # Publisher 
     publisher = Node(package="message_test_demo", executable="message_test_exe")
-
+    # Server in C++
     server_cpp = Node(package="message_test_demo", executable="service_server_exe")
+    # Server in Python
     server_py = Node(package="message_test_demo", executable="service_server_exe.py")
-
+    # Client in C++
     client_subscriber_demo = Node(
         package="message_test_demo",
         executable="service_client_subscriber_exe",
     )
-    
+    # Service calls from timer in C++
     client_timer_demo = Node(
         package="message_test_demo",
         executable="service_client_timer_exe",
